723dd37f4668aa40c39ee376fc40ba356069f2c3
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-04-23 19:54:09 -0300

    Add files via upload

---
+++ Lista.py
@@ -0,0 +1,10 @@
+class List:
+    def nott():
+        a = ["dfa", 543, -43, "g", 32, "fladk", -322, 592, "lvka", "xvc", "03kdalk39",
+             -827, "k39", 302, "d39rk", "0fls93", -7726]
+        
+        print(a)
+
+l = List
+l.nott()        
+input()
